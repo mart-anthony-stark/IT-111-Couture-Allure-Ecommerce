fad2da57c059e3565fdea21e86076d705b6b2cd0
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-23 23:20:59 +0800

    Add: dynamic products rendering using vue.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,31 +1,35 @@
 const products = [
   {
     id: 1,
-    category: "dress",
+    category: "dresses",
     name: "Rib-knit Puff Sleeve Sexy Dress",
     image:
       "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Dresses/1.jpg?raw=true",
+    price: 1200,
   },
   {
     id: 2,
-    category: "dress",
+    category: "dresses",
     name: "Sexy korean plain dress",
     image:
       "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Dresses/2.jpg?raw=true",
+    price: 1200,
   },
   {
     id: 3,
-    category: "dress",
+    category: "dresses",
     name: "Off shoulder maxi korean elegant dress",
     image:
       "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Dresses/3.jpg?raw=true",
+    price: 1200,
   },
   {
     id: 4,
-    category: "dress",
+    category: "dresses",
     name: "ANNIKA Bangkok Floral Ribbon Dress",
     image:
       "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Dresses/4.jpg?raw=true",
+    price: 1200,
   },
   {
     id: 9,
@@ -33,33 +37,38 @@ const products = [
     name: "Korean fashion 2.5 inches heels",
     image:
       "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Shoes/9.jpg?raw=true",
+    price: 1200,
   },
   {
     id: 5,
-    category: "bag",
+    category: "bags",
     name: "Chain fashion Lady crossbody bag",
     image:
       "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Bags/5.jpg?raw=true",
+    price: 1200,
   },
   {
     id: 6,
-    category: "bag",
+    category: "bags",
     name: "Korean Fashion Leather Sling Bag",
     image:
       "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Bags/6.jpg?raw=true",
+    price: 1200,
   },
   {
     id: 7,
-    category: "bag",
+    category: "bags",
     name: "YZ Korean fashion Canvas sling bag",
     image:
       "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Bags/7.jpg?raw=true",
+    price: 1200,
   },
   {
     id: 8,
-    category: "bag",
+    category: "bags",
     name: "Flagship Korean Fashion Shoulder Leather Bag",
     image:
       "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Bags/8.jpg?raw=true",
+    price: 1200,
   },
 ];

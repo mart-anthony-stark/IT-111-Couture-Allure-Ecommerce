21eee786eb02d48ced73facef1dfa1b0ce49c6cb
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-23 22:21:12 +0800

    Add: products data

---
+++ assets/js/data.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    id: 1,
+    category: "dress",
+    name: "Rib-knit Puff Sleeve Sexy Dress",
+    image:
+      "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Dresses/1.jpg?raw=true",
+  },
+  {
+    id: 2,
+    category: "dress",
+    name: "Sexy korean plain dress",
+    image:
+      "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Dresses/2.jpg?raw=true",
+  },
+  {
+    id: 3,
+    category: "dress",
+    name: "Off shoulder maxi korean elegant dress",
+    image:
+      "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Dresses/3.jpg?raw=true",
+  },
+  {
+    id: 4,
+    category: "dress",
+    name: "ANNIKA Bangkok Floral Ribbon Dress",
+    image:
+      "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Dresses/4.jpg?raw=true",
+  },
+  {
+    id: 9,
+    category: "shoes",
+    name: "Korean fashion 2.5 inches heels",
+    image:
+      "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Shoes/9.jpg?raw=true",
+  },
+  {
+    id: 5,
+    category: "bag",
+    name: "Chain fashion Lady crossbody bag",
+    image:
+      "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Bags/5.jpg?raw=true",
+  },
+  {
+    id: 6,
+    category: "bag",
+    name: "Korean Fashion Leather Sling Bag",
+    image:
+      "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Bags/6.jpg?raw=true",
+  },
+  {
+    id: 7,
+    category: "bag",
+    name: "YZ Korean fashion Canvas sling bag",
+    image:
+      "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Bags/7.jpg?raw=true",
+  },
+  {
+    id: 8,
+    category: "bag",
+    name: "Flagship Korean Fashion Shoulder Leather Bag",
+    image:
+      "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Bags/8.jpg?raw=true",
+  },
+];

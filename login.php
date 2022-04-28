f2d15f6965043270fb90ec802e8eb4ca998687a0
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-28 23:57:32 +0800

    Add: footer partial

--- login.php
+++ login.php
@@ -42,53 +42,7 @@
         </div>
     </section>
 
-
-    <footer>
-        <div class="logo">
-            Couture
-            <span>Allure</span>
-        </div>
-        <div class="products-link">
-            <h1 class="col-title">Products</h1>
-            <div class="links">
-                <a href="#">Dresses</a>
-                <a href="#">Shoes</a>
-                <a href="#">Blogs</a>
-                <a href="#">Cosmetics</a>
-            </div>
-        </div>
-        <div class="payments">
-            <h1 class="col-title">We Accept</h1>
-            <div class="flex">
-                <img src="./assets/svg/paypal.svg" alt="">
-                <img src="./assets/svg/visa.svg" alt="">
-            </div>
-            <div class="flex">
-                <img src="./assets/svg/cod.svg" alt="">
-                <img src="./assets/svg/gcash.svg" alt="">
-                <img src="./assets/svg/mastercard.svg" alt="">
-            </div>
-        </div>
-        <div class="connect">
-            <div class="newsletter">
-                <span class="title">Newsletter</span>
-                <div class="email-input">
-                    <input type="email" placeholder="johndoe@gmail.com">
-                    <button>
-                        <i class="fa fa-angle-right" aria-hidden="true"></i>
-                    </button>
-                </div>
-            </div>
-            <div class="socials">
-                <span class="title">Follow us</span>
-                <div class="icons flex">
-                    <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                    <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php require('./_footer.php')?>
 </body>
 
 </html>

c792a07fd981c6acb31f451a00287c13f33fd224
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-01 21:05:07 +0800

    Add: fetching single item

--- assets/js/product.js
+++ assets/js/product.js
@@ -4,12 +4,29 @@ new Vue({
     return {
       item: {
         id: 1,
-        category: "dresses",
-        name: "Rib-knit Puff Sleeve Sexy Dress",
-        image:
-          "https://github.com/mart-anthony-stark/Couture-Allure-Assets/blob/main/Dresses/1.jpg?raw=true",
-        price: 1200,
+        category: "",
+        name: "0",
+        image: "",
+        price: 0,
       },
     };
+  },
+  methods: {
+    getQueryVariable(testVar) {
+      const query = window.location.search.substring(1);
+      const vars = query.split("&");
+      for (let i = 0; i < vars.length; i++) {
+        const pair = vars[i].split("=");
+        if (decodeURIComponent(pair[0]) == testVar) {
+          return decodeURIComponent(pair[1]);
+        }
+      }
+    },
+  },
+  async mounted() {
+    const res = await fetch(`./api/products?id=${this.getQueryVariable("id")}`);
+    if (!res.ok) return (window.location = "./home.php");
+    const data = await res.json();
+    this.item = data[0];
   },
 });
